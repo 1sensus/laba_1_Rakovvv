edb028bf2695070b4cd81986df1514035bef6dc9
1sensus <miraxlxd@gmail.com>
2021-10-03 16:35:26 +0300

    03.10.21
    69 строка - ошибка

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -16,19 +16,20 @@ typedef struct standart_of_nps
 {
 	int id_nps;
 	string name_nps;
-	bool ready_nps;
+	bool ready_nps; 
 }standart_of_nps;
 
 void Print_menu() {
-	system("cls");
+	system("cls");	
 	cout << "What do we want to do?" << endl;
 	cout << "1. Add pipe" << endl;
 	cout << "2. Print pipe" << endl;
 	cout << "3. Print status of pipe" << endl;
 	cout << "4. Add nps" << endl;
 	cout << "5. Print nps" << endl;
-	cout << "6. Exit" << endl;
-	cout << ">";
+	cout << "6. EPrint all nps list" << endl;
+	cout << "8. Exit" << endl;
+	cout << ">";	
 }
 int get_variant(int count) {
 	int digit{ 0 };
@@ -53,13 +54,20 @@ void Print_pipe(standart_of_pipe& s)
 	cout << "++++++++++++++++++++++++++++++++++++++++++++" << endl;
 }
 void Print_nps(standart_of_nps n)
-{
+{	
 	cout << "++++++++++++++++++++++++++++++++++++++++++++" << endl;
 	cout << "+                                          +" << endl;
 	cout << "  id: " << n.id_nps << "    " << "Name: " << n.name_nps << endl
 		<< "  gotovnost': " << n.ready_nps << endl;
 	cout << "+                                          +" << endl;
 	cout << "++++++++++++++++++++++++++++++++++++++++++++" << endl;
+}
+void Print_nps_list(list<standart_of_nps>nps_list, int size_nps_list)
+{	 
+	for (int i = 0; i < size_nps_list; ++i)
+	{
+		Print_nps(nps_list[i]);
+	}
 }
 standart_of_nps Create_nps()
 {
@@ -85,25 +93,23 @@ standart_of_pipe Create_pipe()
 	cin >> s.ready_pipe;
 	return s;
 }
-standart_of_nps Search_by_id(list<standart_of_nps> list_nps, standart_of_nps id_nps)
+/*standart_of_nps Search_by_id(list<standart_of_nps> list_nps, int id)
 {
-	int id = id_nps.id_nps;
-	for (unsigned int i = 0; i < unsigned( list_nps.size()); ++i)
+	int size_nps_list = sizeof(list_nps) / sizeof(standart_of_nps);
+	for (int i = 0; i <size_nps_list; ++i)
 	{
-		if (list_nps[i].id_nps == id) { return list_nps[i]; }
+		if (list_nps[i].id_nps == id) { cout << list_nps[i]; }
 	}
 	cout << "ID ne naiden";
-}
+}*/
 int main()
 {
 	list <standart_of_pipe> pipe_list = {};
 	list <standart_of_nps> nps_list = {};
 	int variant;
-	int size = 0;
-	int list_capacit = 1;
+	int size_nps_list = sizeof(nps_list) / sizeof(standart_of_nps);
 	standart_of_pipe pipe;
 	standart_of_nps nps;
-	Print_menu();
 	do {
 		Print_menu();
 		variant = get_variant(5);
@@ -115,17 +121,21 @@ int main()
 			Print_pipe(pipe);
 			break;
 		case 3:
-			cout << "Status of pipe number" /*Vedetsya ID*/ << endl;
+			cout << "Status of pipe number" << endl;
 			break;
 		case 4:
 			nps = Create_nps();
+			nps_list.push_back(nps);
 			break;
 		case 5:
 			Print_nps(nps);
 			break;
+		case 6:
+			Print_nps_list(nps_list, size_nps_list);
+			break;
 		}
-		if (variant != 6)
+		if (variant != 8)
 			system("pause");
-	} while (variant != 6);
+	} while (variant != 8);
 }
 
