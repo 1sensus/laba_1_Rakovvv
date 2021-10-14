046ee8f5570e91b1df0413973d749d2372e13d96
1sensus <miraxlxd@gmail.com>
2021-10-14 15:38:31 +0300

    краш с интом
    проверка

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,21 +7,21 @@
 #include <string>
 
 /*ЛР 1
-Создать консольное приложение, описывающая базовые сущности трубопроводного транспорта 
+Создать консольное приложение, описывающая базовые сущности трубопроводного транспорта
 газа или нефти: труба и КС или НПС (в сильно упрощенном варианте).
-Свойства трубы: идентификатор, длина, диаметр, признак "в ремонте". 
+Свойства трубы: идентификатор, длина, диаметр, признак "в ремонте".
 Свойства КС: идентификатор, название, количество цехов, количество цехов в работе,
 эффективность (некий показатель, обобщающий различные специфические характеристики)
 Для каждой структуры реализовать операции: считывание с консоли или из файла,
 вывод на консоль или в файл, редактирование признака "в ремонте" для трубы, запуск и останов цеха в КС,
 сохранение всей информации в файл, загрузка информации из файла.
 При запуске программы выводится меню, запрашивающее в бесконечном цикле действие
-от пользователя. 
+от пользователя.
 Пример меню: 1. Добавить трубу 2. Добавить КС 3. Просмотр всех объектов 4. Редактировать трубу 5. Редактировать КС 6. Сохранить 7. Загрузить 0. Выход
 Поддержка русского языка не обязательна.
 Обязательно: проверка корректности совершаемых действий.
 Программа должна быть устойчива к любым действиям пользователя.
-Повод для снижения балла: бессмысленное наименование переменных и функций, 
+Повод для снижения балла: бессмысленное наименование переменных и функций,
 необоснованное использование глобальных переменных, неструктурированный код.
 Ход выполнения работы фиксируется коммитами в github (с осмысленными комментариями).
 К ответу прикрепляется ссылка на финальный коммит! */
@@ -44,7 +44,7 @@ typedef struct standart_of_nps
 	string name_nps;
 	double all_parts;
 	double warking_parts;
-	double kpd; 
+	double kpd;
 }standart_of_nps;
 int get_variant(int a)
 {
@@ -65,11 +65,11 @@ int get_variant(int a)
 	return number;
 }
 void Print_menu() {
-	system("cls");	
+	system("cls");
 	cout << "What do we want to do?" << endl;
 	cout << "1. Add pipe" << endl;
 	cout << "2. Add nps" << endl;
-	cout << "3. Print all objects" <<endl;
+	cout << "3. Print all objects" << endl;
 	cout << "4. Change status pipe" << endl;
 	cout << "5. Change status nps" << endl;
 	cout << "6. Save in file" << endl;
@@ -78,23 +78,23 @@ void Print_menu() {
 }
 void Print_pipe(standart_of_pipe& s)
 {
-		cout << "  id: " << s.id_pipe << "  |  " << "deametr: " << s.deametr_pipe << "  |  " << "  dlina: " << s.long_pipe << "  |  "
+	cout << "  id: " << s.id_pipe << "  |  " << "deametr: " << s.deametr_pipe << "  |  " << "  dlina: " << s.long_pipe << "  |  "
 		<< "  gotovnost': " << s.ready_pipe << endl;
 }
 void Print_nps(standart_of_nps n)
-{	
+{
 	cout << "  id: " << n.id_nps << "  |  " << "Name: " << n.name_nps << "  |  " << "efectivnost'': " << n.kpd
 		<< " %(" << n.warking_parts << " / " << n.all_parts << ")" << endl;
 }
 void Print_all_objects(vector <standart_of_nps> nps_vector,
 	vector<standart_of_pipe> pipe_vector)
-{	
-	cout << "Npss: "<<endl;
-	for (auto& nps : nps_vector) 
+{
+	cout << "Npss: " << endl;
+	for (auto& nps : nps_vector)
 	{
 		Print_nps(nps);
 	}
-	cout << "Pipes: "<<endl;
+	cout << "Pipes: " << endl;
 	for (auto& pipe : pipe_vector)
 	{
 		Print_pipe(pipe);
@@ -112,7 +112,7 @@ int Cheke_ID_pipe(int s_id, vector<standart_of_pipe> pipe_vector)
 	}
 	return cheker;
 }
-int Cheke_ID_nps(int n_id, vector <standart_of_nps>nps_vector) 
+int Cheke_ID_nps(int n_id, vector <standart_of_nps>nps_vector)
 {
 	int cheker = 0;
 	for (auto& nps : nps_vector)
@@ -128,24 +128,24 @@ standart_of_nps Create_nps(vector<standart_of_nps> nps_vector)
 {
 	standart_of_nps n = {};
 	cout << "Vvedite ID nps";
-	cin >> n.id_nps;	
+	cin >> n.id_nps;
 	int cheke_id = Cheke_ID_nps(n.id_nps, nps_vector);
-		while (cheke_id != 0)
-		{
-			cout << "Danni' ID zanyat, vvedite drugoi'" << endl;
-			cin >> n.id_nps;
-			cheke_id = Cheke_ID_nps(n.id_nps, nps_vector);
-		}
+	while (cheke_id != 0)
+	{
+		cout << "Danni' ID zanyat, vvedite drugoi'" << endl;
+		cin >> n.id_nps;
+		cheke_id = Cheke_ID_nps(n.id_nps, nps_vector);
+	}
 	cout << "Vvedite imya nps";
 	cin.ignore();
 	getline(cin, n.name_nps);
 	cout << "Vvedite kol-vo rabotaushih cehov" << endl;
 	cin >> n.warking_parts;
-	cout << "Vvedite obshee kol-vo cehov "<<endl;
+	cout << "Vvedite obshee kol-vo cehov " << endl;
 	cin >> n.all_parts;
 	while (n.all_parts < n.warking_parts)
 	{
-		cout << "Oshibka vvoda. Vvedite korrektnii dannii"<<endl;
+		cout << "Oshibka vvoda. Vvedite korrektnii dannii" << endl;
 
 		cout << "Vvedite kol-vo rabotaushih cehov" << endl;
 		cin >> n.warking_parts;
@@ -160,13 +160,13 @@ standart_of_pipe Create_pipe(vector<standart_of_pipe> pipe_vector)
 	standart_of_pipe s = {};
 	cout << "Vvedite ID trubi'";
 	cin >> s.id_pipe;
-	int cheke_id = Cheke_ID_pipe(s.id_pipe,pipe_vector);
-		while (cheke_id!=0)
-		{
-			cout << "Danni' ID zanyat, vvedite drugoi'"<<endl;
-			cin >> s.id_pipe;
-			cheke_id = Cheke_ID_pipe(s.id_pipe, pipe_vector);
-		}
+	int cheke_id = Cheke_ID_pipe(s.id_pipe, pipe_vector);
+	while (cheke_id != 0)
+	{
+		cout << "Danni' ID zanyat, vvedite drugoi'" << endl;
+		cin >> s.id_pipe;
+		cheke_id = Cheke_ID_pipe(s.id_pipe, pipe_vector);
+	}
 	cout << "Vvedite deametr trubi'";
 	cin >> s.deametr_pipe;
 	cout << "Vvedite dliny trubi'";
@@ -178,30 +178,30 @@ standart_of_pipe Create_pipe(vector<standart_of_pipe> pipe_vector)
 void Chenge_status_search_by_id_nps(vector <standart_of_nps>& nps_vector)
 {
 	int search_id;
-	int cheker=0;
+	int cheker = 0;
 	auto i = 0;
-	cout << "Vvedite ID"<<endl;
+	cout << "Vvedite ID" << endl;
 	cin >> search_id;
 	for (auto& nps : nps_vector)
 	{
 		++i;
-		if (nps.id_nps == search_id) 
+		if (nps.id_nps == search_id)
 		{
-			 ++cheker;			
-			 cout << "Vvedite novoe kol-vo rabotaushih cehov: " << endl;
-			 cin >> nps.warking_parts;
-			 while (nps.all_parts < nps.warking_parts)
-			 {			 
+			++cheker;
+			cout << "Vvedite novoe kol-vo rabotaushih cehov: " << endl;
+			cin >> nps.warking_parts;
+			while (nps.all_parts < nps.warking_parts)
+			{
 
-				 cout << "Vvedite kol-vo rabotaushih cehov" << endl;
-				 cin >> nps.warking_parts;
-				 cout << "Vvedite obshee kol-vo cehov " << endl;
-				 cin >> nps.all_parts;
-			 }
-			 nps.kpd = nps.warking_parts / nps.all_parts * 100;
-			 break;
+				cout << "Vvedite kol-vo rabotaushih cehov" << endl;
+				cin >> nps.warking_parts;
+				cout << "Vvedite obshee kol-vo cehov " << endl;
+				cin >> nps.all_parts;
+			}
+			nps.kpd = nps.warking_parts / nps.all_parts * 100;
+			break;
 		}
-	} if(cheker == 0)cout << "ID ne naiden";
+	} if (cheker == 0)cout << "ID ne naiden";
 	else { cout << "Izmeneniya sohraneni'"; }
 }
 void Chenge_status_search_by_id_pipe(vector <standart_of_pipe>& pipe_vector)
@@ -216,35 +216,17 @@ void Chenge_status_search_by_id_pipe(vector <standart_of_pipe>& pipe_vector)
 		++i;
 		if (pipe.id_pipe == search_id)
 		{
-			++cheker;			
+			++cheker;
 			cout << "Vvedite novi' status: " << endl;
-		    cin >> pipe.ready_pipe;
+			cin >> pipe.ready_pipe;
 		}
 	} if (cheker == 0)cout << "ID ne naiden";
 	else { cout << "Izmeneniya sohraneni'"; }
 }
-void file_nps(vector<standart_of_nps>& nps_vector)
-{	
-	fstream file_out;
-	file_out.open("Npss.txt", fstream::out);
-	if (!file_out.is_open())
-	{
-		cout << "Error!!!" << endl;
-	}
-	else
-	{
-		for (auto& nps : nps_vector)
-		{
-			file_out << nps.id_nps << "|" << nps.name_nps << "|"<<nps.kpd<<
-				"|"<<nps.warking_parts<<"|"<<nps.all_parts<<"|" << endl;
-		}
-	}
-	file_out.close();
-}
-void file_pipe(vector<standart_of_pipe>& pipe_vector)
+void file_outer(vector <standart_of_pipe>& pipe_vector, vector<standart_of_nps>&nps_vector)
 {
 	fstream file_out;
-	file_out.open("Pipes.txt", fstream::out);
+	file_out.open("All.txt", fstream::out);
 	if (!file_out.is_open())
 	{
 		cout << "Error!!!" << endl;
@@ -253,75 +235,65 @@ void file_pipe(vector<standart_of_pipe>& pipe_vector)
 	{
 		for (auto& pipe : pipe_vector)
 		{
-			file_out << pipe.id_pipe << "|" << pipe.deametr_pipe << "|"
-				<< pipe.long_pipe << "|" << pipe.ready_pipe << "|"<<endl;
+			file_out << pipe.id_pipe<<endl<<pipe.deametr_pipe <<endl<< pipe.long_pipe<<endl<<pipe.ready_pipe<< endl;
 		}
+		file_out << "/"<< endl;
+		for (auto& nps : nps_vector)
+		{
+			file_out << nps.id_nps << endl << nps.name_nps << endl << nps.warking_parts << endl << nps.all_parts<< endl << nps.kpd;
+		}
+
 	}
 	file_out.close();
 }
-void read_nps(vector <standart_of_nps>& nps_vector)
+
+void read_file(vector <standart_of_pipe>& pipe_vector,vector<standart_of_nps>&nps_vector)
 {
+	standart_of_pipe pipe = {};
 	standart_of_nps nps = {};
 	ifstream read_file;
-	read_file.open("Npss.txt");
-		if (!read_file.is_open()) { cout << "Error!!!"; }
-		else
-		{
-			string  part_of_line,id_s,name_s,kpd_s,works_s,all_s;
-			cout << "opened"<<endl;	
-			int i = 0;
-			while (!read_file.eof())
-			{
-				++i;
-				getline(read_file, part_of_line);
-				size_t _1st;
-				_1st= part_of_line.find('|');
-				id_s = part_of_line.substr(0,_1st);
-				part_of_line.erase(0, _1st+1);
-				_1st = part_of_line.find('|');
-				nps.name_nps= part_of_line.substr(0, _1st);
-				part_of_line.erase(0, _1st + 1);
-				_1st = part_of_line.find('|');
-				kpd_s = part_of_line.substr(0, _1st);
-				part_of_line.erase(0, _1st + 1);
-				_1st = part_of_line.find('|');
-				works_s = part_of_line.substr(0, _1st);
-				part_of_line.erase(0, _1st + 1);
-				_1st = part_of_line.find('|');
-				all_s = part_of_line.substr(0, _1st);
-				part_of_line.erase(0, _1st + 1);
-
-				nps.id_nps = stoi(id_s);
-				nps.kpd = atof(kpd_s.c_str());
-				nps.warking_parts = atof(works_s.c_str());
-				nps.all_parts = atof(all_s.c_str());
-				nps_vector.push_back(nps);
-
-				/*_2nd = part_of_line.find('|');
-				name_s = part_of_line.substr(_1st,_2nd);
-				part_of_line.erase(_2nd);
-				_1st = part_of_line.find('|');
-				kpd_s = part_of_line.substr(0, _1st);
-				part_of_line.erase(0, _1st+1);
-				nps.id_nps = stoi(id_s);
-				nps.name_nps = name_s;
-				nps.kpd = atof(kpd_s.c_str());
-				nps_vector.push_back(nps);*/										
+	read_file.open("All.txt",ifstream::in);
+	if (!read_file.is_open()) { cout << "Error!!!"; }
+	else
+	{
+		while(!read_file.eof()){
+		bool delimitr= false;
+		string  part_of_line;
+		cout << "opened" << endl;
+		getline(read_file, part_of_line);
+		if (part_of_line == "/") {
+			delimitr = true;
+		}
+		if (delimitr ==false)
+		{	
+			read_file >> pipe.id_pipe;
+			read_file >> pipe.deametr_pipe;
+			read_file >> pipe.long_pipe;
+			read_file >> pipe.ready_pipe;
+			pipe_vector.push_back(pipe);
+			
+		}
+		else {
+			read_file >> nps.id_nps;
+			read_file >> nps.name_nps;
+			read_file >> nps.warking_parts;
+			read_file >> nps.all_parts;
+			read_file >> nps.kpd;
+			nps_vector.push_back(nps);
 			}
 		}
+		
+			
+			
+		
+	}
 	read_file.close();
-	/*	
-	nps.id_nps = stoi(part_of_line);
-	nps.name_nps = part_of_line;
-	nps.kpd = atof(part_of_line.c_str())
-	nps_vector.push_back(nps);;
-	*/
 }
 
 int main()
-{	
+{
 	vector < standart_of_nps > nps_vector = {};
-	vector <standart_of_pipe> pipe_vector = {};	
+	vector <standart_of_pipe> pipe_vector = {};
 	int size_nps_vector = sizeof(nps_vector) / sizeof(standart_of_nps);
 	standart_of_pipe pipe;
 	standart_of_nps nps;
@@ -337,13 +309,13 @@ int main()
 		case 2:
 			nps = Create_nps(nps_vector);
 			nps_vector.push_back(nps);
-			break;			
+			break;
 		case 3:
 			cout << "List of all objects" << endl;
 			Print_all_objects(nps_vector, pipe_vector);
 			break;
 		case 4:
-			cout << "Vvedite ID trubi', kotoryu hotite izmenit'"<<endl;
+			cout << "Vvedite ID trubi', kotoryu hotite izmenit'" << endl;
 			Chenge_status_search_by_id_pipe(pipe_vector);
 			break;
 		case 5:
@@ -351,24 +323,15 @@ int main()
 			Chenge_status_search_by_id_nps(nps_vector);
 			break;
 		case 6:
-			{
-			cout << "1.Zapisat' pipes" << endl << "2.Zapisat' npss"<<endl;
-			int x;
-				cin >> x;
-			while ((x != 1) && (x != 2)) 
-			{
-				cout << "Vvedite correktni otvet";
-			}
-			if (x == 1) { file_pipe(pipe_vector); }
-			else { file_nps(nps_vector); }
-			
+		
+			file_outer(pipe_vector, nps_vector);
 			break;
-			}
+		
 		case 7:
-			read_nps(nps_vector);
+			read_file(pipe_vector,nps_vector);
 			break;
 		}
 		if (variant != 8)
 			system("pause");
 	} while (variant != 8);
-} 
+}
