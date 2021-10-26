7941686a74cf3e5b51c16735011907eb7586b11c
1sensus <miraxlxd@gmail.com>
2021-10-26 15:26:08 +0300

    реализовать фильтры

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,28 +6,22 @@
 #include <cstring>
 #include <string>
 
-/*ЛР 1
-Создать консольное приложение, описывающая базовые сущности трубопроводного транспорта
-газа или нефти: труба и КС или НПС (в сильно упрощенном варианте).
-Свойства трубы: идентификатор, длина, диаметр, признак "в ремонте".
-Свойства КС: идентификатор, название, количество цехов, количество цехов в работе,
-эффективность (некий показатель, обобщающий различные специфические характеристики)
-Для каждой структуры реализовать операции: считывание с консоли или из файла,
-вывод на консоль или в файл, редактирование признака "в ремонте" для трубы, запуск и останов цеха в КС,
-сохранение всей информации в файл, загрузка информации из файла.
-При запуске программы выводится меню, запрашивающее в бесконечном цикле действие
-от пользователя.
-Пример меню: 1. Добавить трубу 2. Добавить КС 3. Просмотр всех объектов 4. Редактировать трубу 5. Редактировать КС 6. Сохранить 7. Загрузить 0. Выход
-Поддержка русского языка не обязательна.
-Обязательно: проверка корректности совершаемых действий.
-Программа должна быть устойчива к любым действиям пользователя.
-Повод для снижения балла: бессмысленное наименование переменных и функций,
-необоснованное использование глобальных переменных, неструктурированный код.
-Ход выполнения работы фиксируется коммитами в github (с осмысленными комментариями).
-К ответу прикрепляется ссылка на финальный коммит! */
+/*
+https://github.com/papilinatm/cpp_lessons_2020/blob/master/cpp_lessons/main.cpp
+Изменить программу из предыдущей работы:
 
-//vector,имя через пробел,передача по ссылке и значению,
-//проверка ввода,сохранение и загрузка в из файла
+1)добавить работу с несколькими объектами (несколько труб, несколько КС, с возможностью добавления, редактирования, удаления);
+2)обеспечить уникальную идентификацию объектов;
+3)поиск объектов по заданному фильтру:
+a)трубы: по названию, по признаку «в ремонте»;
+b)КС: по названию, по проценту незадействованных цехов;
+4)пакетное редактирование труб (т.е. редактирование или всех найденных, или по выбору пользователя)
+ UPD: название файлов для сохранения/загрузки задаются пользователем.
+Пакетное редактирование - это отредактировать все элементы во множестве (например, все найденные) или подмножество (пользователь указывает, какие именно).
+
+Для претендующих на высокий балл: разумное использование ресурсов, грамотная организация кода
+(убрать излишнее дублирование кода, каждая функция выполняет небольшое, логически законченное действие).
+Продемонстрировать умение и понимание работы с классами, ссылками, потоками ввода/вывода.*/
 using namespace std;
 
 
@@ -74,20 +68,34 @@ void Print_menu() {
 	cout << "5. Change status nps" << endl;
 	cout << "6. Save in file" << endl;
 	cout << "7. Load from file" << endl;
-	cout << "8. Exit" << endl;
+	cout << "8. Delete nps by name " << endl;
+	cout << "9. Search by filter" << endl;
+	cout << "0. Exit" << endl;
 }
-void Print_pipe(const standart_of_pipe& s)/////////////////////////
+void Print_menu_for_filter()
+{
+	system("cls");
+	cout << "Searching by filter" << endl;
+	cout << "1." << endl;
+	cout << "2." << endl;
+	cout << "3." << endl;
+	cout << "4." << endl;
+	cout << "5." << endl;
+	cout << "0. Back" << endl;
+
+}
+void Print_pipe(const standart_of_pipe& s)
 {
 	cout << "  id: " << s.id_pipe << "  |  " << "deametr: " << s.diameter_pipe << "  |  " << "  dlina: " << s.long_pipe << "  |  "
 		<< "  gotovnost': " << s.ready_pipe << endl;
 }
-void Print_nps(const standart_of_nps& n)//////////////////////////////
+void Print_nps(const standart_of_nps& n)
 {
 	cout << "  id: " << n.id_nps << "  |  " << "Name: " << n.name_nps << "  |  " << "zanyatost': " << n.kpd
 		<< " %(" << n.working_parts << " / " << n.all_parts << ")" << endl;
 }
-void Print_all_objects(const vector <standart_of_nps>& nps_vector,const	vector<standart_of_pipe>& pipe_vector)//////////////////////////////////////
-{	
+void Print_all_objects(const vector <standart_of_nps>& nps_vector, const	vector<standart_of_pipe>& pipe_vector)
+{
 	cout << "Pipes: " << endl;
 	for (auto& pipe : pipe_vector)
 	{
@@ -99,16 +107,16 @@ void Print_all_objects(const vector <standart_of_nps>& nps_vector,const	vector<s
 		Print_nps(nps);
 	}
 }
-bool Check_ID_pipe(int s_id,const vector<standart_of_pipe>& pipe_vector)//////////////////////////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+bool Check_ID_pipe(int s_id, const vector<standart_of_pipe>& pipe_vector)
 {
 	for (auto& pipe : pipe_vector)
 		if (pipe.id_pipe == s_id)
 			return false;
 	return true;
 }
-bool Check_ID_nps(int n_id,const vector <standart_of_nps>& nps_vector)/////////////////////////////////////
+bool Check_ID_nps(int n_id, const vector <standart_of_nps>& nps_vector)
 {
-	for (auto& nps : nps_vector)	
+	for (auto& nps : nps_vector)
 		if (nps.id_nps == n_id)
 			return true;
 	return false;
@@ -116,7 +124,7 @@ bool Check_ID_nps(int n_id,const vector <standart_of_nps>& nps_vector)//////////
 standart_of_nps Create_nps(int nps_id)
 {
 	standart_of_nps n = {};
-	n.id_nps = nps_id;	
+	n.id_nps = nps_id;
 	cout << "Vvedite imya nps";
 	cin.ignore();
 	getline(cin, n.name_nps);
@@ -139,7 +147,7 @@ standart_of_nps Create_nps(int nps_id)
 standart_of_pipe Create_pipe(int pipe_id)
 {
 	standart_of_pipe s = {};
-	s.id_pipe=pipe_id;	
+	s.id_pipe = pipe_id;
 	cout << "Vvedite diameter trubi'";
 	cin >> s.diameter_pipe;
 	cout << "Vvedite dliny trubi'";
@@ -160,7 +168,7 @@ void Chenge_status_search_by_id_nps(vector <standart_of_nps>& nps_vector)
 		++i;
 		if (nps.id_nps == search_id)
 		{
-			check=true;
+			check = true;
 			cout << "Vvedite novoe kol-vo rabotaushih cehov: " << endl;
 			cin >> nps.working_parts;
 			while (nps.all_parts < nps.working_parts)
@@ -189,7 +197,7 @@ void Chenge_status_search_by_id_pipe(vector <standart_of_pipe>& pipe_vector)
 		++i;
 		if (pipe.id_pipe == search_id)
 		{
-			check=true;
+			check = true;
 			cout << "Vvedite novi' status: " << endl;
 			cin >> pipe.ready_pipe;
 		}
@@ -208,16 +216,16 @@ void file_outer(const vector <standart_of_pipe>& pipe_vector, const vector<stand
 	{
 		size_t a = size(pipe_vector);
 		size_t b = size(nps_vector);
-		file_out << a<<endl;
+		file_out << a << endl;
 		file_out << b << endl;
 
 		for (auto& pipe : pipe_vector)
 		{
-			file_out<< pipe.id_pipe << endl << pipe.diameter_pipe << endl << pipe.long_pipe << endl << pipe.ready_pipe << endl;
+			file_out << pipe.id_pipe << endl << pipe.diameter_pipe << endl << pipe.long_pipe << endl << pipe.ready_pipe << endl;
 		}
 		for (auto& nps : nps_vector)
 		{
-			file_out << nps.id_nps << endl << nps.name_nps << endl << nps.working_parts << endl << nps.all_parts << endl << nps.kpd<<endl;
+			file_out << nps.id_nps << endl << nps.name_nps << endl << nps.working_parts << endl << nps.all_parts << endl << nps.kpd << endl;
 		}
 		cout << "writen";
 
@@ -240,7 +248,7 @@ void read_file(vector <standart_of_pipe>& pipe_vector, vector<standart_of_nps>& 
 		read_file >> kol_vo_npss;
 		while (!read_file.eof())
 		{
-			for(int i=0;i<kol_vo_pipes;++i)
+			for (int i = 0; i < kol_vo_pipes; ++i)
 			{
 				standart_of_pipe pipe = {};
 				read_file >> pipe.id_pipe;
@@ -264,6 +272,28 @@ void read_file(vector <standart_of_pipe>& pipe_vector, vector<standart_of_nps>& 
 		}read_file.close();
 	}cout << "file read" << endl;
 }
+int Search_by_name_nps(vector<standart_of_nps>nps_vector, string name)
+{
+	for (auto nps : nps_vector)
+	{
+		if (nps.name_nps == name)
+			return nps.id_nps;
+	}
+}
+void Delete_elem(vector<standart_of_nps>& nps_vector)
+{
+	string name_of_nps;
+	cout << "Vvedite imya trubi" << endl;
+	getline(cin, name_of_nps);
+	try
+	{
+		int id_of_delete_element = Search_by_name_nps(nps_vector, name_of_nps);
+		nps_vector.erase(nps_vector.begin() + id_of_delete_element);
+	}
+	catch (int a) { cout << "Imya ne naideno" << endl; }
+}
+
+
 int main()
 {
 	vector < standart_of_nps > nps_vector = {};
@@ -272,16 +302,19 @@ int main()
 	int pipe_counter = 0;
 	int nps_counter = 0;
 	int variant;
-	do {
+	do 
+	{
 		Print_menu();
 		variant = get_variant();
 		switch (variant) {
-		case 1:			
+		case 1:
+		{
 			auto pipe = Create_pipe(pipe_counter);
 			if (pipe.diameter_pipe == 0 || pipe.long_pipe == 0) { cout << " !!Error input,retry!!"; break; }
 			pipe_vector.push_back(pipe);
 			pipe_counter++;
 			break;
+		}
 		case 2:
 		{
 			auto nps = Create_nps(nps_counter);
@@ -291,27 +324,64 @@ int main()
 			break;
 		}
 		case 3:
+		{
 			cout << "List of all objects" << endl;
 			Print_all_objects(nps_vector, pipe_vector);
 			break;
+		}
 		case 4:
+		{
 			cout << "Vvedite ID trubi', kotoryu hotite izmenit'" << endl;
 			Chenge_status_search_by_id_pipe(pipe_vector);
 			break;
+		}
 		case 5:
+		{
 			cout << "Vvedite ID nps, kotoryu hotite izmenit'" << endl;
 			Chenge_status_search_by_id_nps(nps_vector);
 			break;
+		}
 		case 6:
-
+		{
 			file_outer(pipe_vector, nps_vector);
 			break;
 
+		}
 		case 7:
+		{
 			read_file(pipe_vector, nps_vector);
 			break;
 		}
-		if (variant != 8)
-			system("pause");
-	} while (variant != 8);
+		case 8:
+		{
+			cout << "Delete" << endl;
+			Delete_elem(nps_vector);
+		}
+			case 9:
+			{
+				int variant2;
+				do {
+						Print_menu_for_filter();
+						variant2 = get_variant();
+						switch (variant2)
+						{
+							case 1:
+							{
+								cout << "1" << endl;
+								break;
+							}				
+							case 2:
+							{
+								cout << "2" << endl;
+								break;
+							}
+
+						}
+					if (variant2 != 0)system("pause");
+				} while (variant2 != 0);
+			}
+		}
+		if (variant != 0)system("pause");
+	} while (variant != 0);
 }
+
